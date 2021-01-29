866d69eca99446fec41bd6cf533c3920c2230c6d
Alejandro <alex.ggperez@gmail.com>
2021-01-29 14:31:45 +0100

    sumbit pressing enter key

--- src/components/ChatContainer/ChatFooter/ChatFooter.js
+++ src/components/ChatContainer/ChatFooter/ChatFooter.js
@@ -8,19 +8,27 @@ const ChatFooter = (props) => {
 
   const textInput = useRef(null);
 
-  const clickHandler = () => {
+  const submitHandler = () => {
     const inputText =  textInput.current.value;
     textInput.current.value = '';
     props.send(inputText);
-  }
+  };
+
+  const handleKeypress = e => {
+    //it triggers by pressing the enter key
+    if (e.charCode === 13) {
+      submitHandler();
+    }
+  };
 
   return (
     <div className={classes.ChatFooter}>
       <input 
         type="text"
         placeholder="Escribe algo aquí..."
+        onKeyPress={handleKeypress}
         ref={textInput}/>
-      <div className={classes.SendButton} onClick={clickHandler}>
+      <div className={classes.SendButton} onClick={submitHandler}>
         <img src={sendImage} alt='Close'/>
       </div>
     </div>
