c560958f46c028843e19a7984a2177dc4e73f5e7
Alejandro <alex.ggperez@gmail.com>
2021-02-02 22:25:03 +0100

    carousel scroll arrows working

--- src/components/chatElements/Carousel/Carousel.js
+++ src/components/chatElements/Carousel/Carousel.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { Component, createRef } from 'react';
 
 import classes from './Carousel.module.css';
 import arrowIcon from '../../../assets/images/arrow-icon.png';
@@ -6,9 +6,44 @@ import Card from '../Card/Card';
 
 class Carousel extends Component {
 
+  constructor(props) {
+    super(props);
+    this.itemsContainerRef = createRef();
+  }
+  
   state={
-    showLeftArroy: true,
+    showLeftArroy: false,
     showRightArrow: true
+  }
+
+  mouseDown= null;
+
+  
+  mouseUpHandler = () =>{
+    clearInterval(this.mouseDown);
+  }
+
+  scrollLeftHandler = () =>{
+    this.mouseDown = setInterval(()=>{
+        this.itemsContainerRef.current.scrollLeft = this.itemsContainerRef.current.scrollLeft - 5;
+        if(this.itemsContainerRef.current.scrollLeft <=0 ) {
+          this.setState({showLeftArroy: false});
+          this.mouseUpHandler(); // mouseUp cause button desapears and mouseUp event deseapears as well
+        }
+        if(this.itemsContainerRef.current.scrollLeft < this.itemsContainerRef.current.scrollWidth) this.setState({showRightArrow: true})
+    },5);
+  }
+
+  scrollRightHandler = () =>{
+    this.mouseDown = setInterval(()=>{
+      console.log(this.itemsContainerRef.current.scrollLeft,this.itemsContainerRef.current.scrollWidth)
+      this.itemsContainerRef.current.scrollLeft = this.itemsContainerRef.current.scrollLeft + 5;
+      if(this.itemsContainerRef.current.scrollLeft > 0) this.setState({showLeftArroy: true});
+      if(this.itemsContainerRef.current.scrollLeft >= this.itemsContainerRef.current.scrollWidth-this.itemsContainerRef.current.clientWidth) {
+        this.setState({showRightArrow: false})
+        this.mouseUpHandler(); // mouseUp cause button desapears and mouseUp event deseapears as well
+      }
+    },5);
   }
 
   render(){
@@ -25,17 +60,22 @@ class Carousel extends Component {
         payload={item.payload}
         selected = {this.props.selected} />
  
-      
     ));
 
     const leftArrow = (
-      <div className={[classes.Arrow, classes.ArrowLeft].join(' ')}> 
+      <div 
+        className={[classes.Arrow, classes.ArrowLeft].join(' ')}
+        onMouseDown={this.scrollLeftHandler}
+        onMouseUp={this.mouseUpHandler}> 
         <img src={arrowIcon} alt=''/>
       </div>
     )
 
     const rightArrow = (
-      <div className={[classes.Arrow, classes.ArrowRight].join(' ')}> 
+      <div 
+        className={[classes.Arrow, classes.ArrowRight].join(' ')}
+        onMouseDown={this.scrollRightHandler}
+        onMouseUp={this.mouseUpHandler}> 
         <img src={arrowIcon} alt=''/>
       </div>
     )
@@ -43,7 +83,7 @@ class Carousel extends Component {
     return (
       <div className={messageClasses.join(' ')}>
         {this.state.showLeftArroy ? leftArrow : ''}
-        <div className={classes.ItemsContainer}>
+        <div className={classes.ItemsContainer} ref={this.itemsContainerRef} >
           {items}
         </div>
         {this.state.showRightArrow ? rightArrow : ''}
